c4a28d88e4ac50bcac79705507fd9f9548f84213
Kaeneth Dayao <kendayao@gmail.com>
2020-12-14 14:47:31 -0800

    header component styled

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,47 +1,60 @@
-import React from 'react'
+import React from "react";
+import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
-import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";;
-
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import {Link} from "react-router-dom";
+import {useStateValue} from '../../contextAPI/StateProvider';
+import {auth} from '../../firebase/firebase'
 
 function Header() {
-    return (
-        <div className="header">
-             <img
-            className="header__logo"
-            src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-            alt="logo"
-            />
+  const[{basket, user},dispatch]=useStateValue();
 
-        <div className="header__search">
-            <input className="header__searchInput" type="text" />
-            <SearchIcon className="header__searchIcon" />
-        </div>
-        <div className="header__nav">
-            
-            <div onClick={handleAuthentication} className="header__option">
-                <span className="header__optionLineOne">{user ? 'Hello, ' + user?.email: 'Hello Guest'}</span>
-                <span className="header__optionLineTwo">{user? 'Sign Out' : 'Sign In'}</span>
-            </div>
-           
-            
-            <div className="header__option">
-                <span className="header__optionLineOne">Returns</span>
-                <span className="header__optionLineTwo">& Orders</span>
-            </div>
-         
-            <div className="header__option">
-            <span className="header__optionLineOne">Your</span>
-            <span className="header__optionLineTwo">Prime</span>
-            </div>
-            
-            <div className="header__optionBasket">
-                <ShoppingBasketIcon />
-                <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
-            </div>
-            
+  const handleAuthentication=()=>{
+    if(user){
+      auth.signOut();
+    }
+  }
+  return (
+    <div className="header">
+      <Link to="/">
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt="logo"
+      />
+      </Link>
+      
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
       </div>
+      <div className="header__nav">
+        <Link className="header__link" to={!user&&'/login'}>
+          <div onClick={handleAuthentication} className="header__option">
+            <span className="header__optionLineOne">{user ? 'Hello, ' + user?.email: 'Hello Guest'}</span>
+            <span className="header__optionLineTwo">{user? 'Sign Out' : 'Sign In'}</span>
+          </div>
+        </Link>
+        <Link className="header__link"  to='/orders'>
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
         </div>
-    )
+        <Link className="header__link" to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
+
