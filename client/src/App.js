9719ed3a7c3a208a5f6fa8f01d6d64ed1030c1fd
Kaeneth Dayao <kendayao@gmail.com>
2020-12-14 14:23:17 -0800

    added header component and added logo to header

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="app">
-     
+     <Header />
     </div>
   );
 }
---
+++ client/src/components/header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import SearchIcon from "@material-ui/icons/Search";
+
+
+function Header() {
+    return (
+        <div className="header">
+             <img
+            className="header__logo"
+            src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+            alt="logo"
+            />
+
+        <div className="header__search">
+            <input className="header__searchInput" type="text" />
+            <SearchIcon className="header__searchIcon" />
+        </div>
+        
+        </div>
+    )
+}
+
+export default Header
